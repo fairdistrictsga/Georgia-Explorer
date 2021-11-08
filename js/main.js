23d4b5076186e70615fecda640a987cfbdec573e
skhaji <sara.hodges@gmail.com>
2021-11-08 14:37:50 -0500

    adding new house map, 11/8

--- js/main.js
+++ js/main.js
@@ -349,6 +349,37 @@ map.on("mousemove", "house_p2_popup", function(e) {
     }
 });
 
+map.on("mousemove", "house_p3_popup", function(e) {
+  $('#sidebar').show();
+// create hover effect
+  map.setFilter("house_p3_hover", ["==", "district", e.features[0].properties.district]);
+
+// change cursor to pointer
+  map.getCanvas().style.cursor = 'pointer';
+
+    var features = map.queryRenderedFeatures(e.point, {
+        layers: ["house_p3_popup"]
+    });
+
+    if (features.length) {
+
+        document.getElementById('tooltip').innerHTML = 
+        '<h3>' + '<strong>' + 'Map proposed by House Committee Chair, Nov. 8, 2021' + '</strong>' + '</h3>' +
+        '<h3>' + '<strong>' + 'Proposed ' + '</strong>' + 'State House District ' + e.features[0].properties.district + '</h3>' +
+                  'Population, 2020: ' + '<strong>' + e.features[0].properties.pop.toLocaleString("en-US") + '</strong>' +
+        '</br>' + 'Voting Age Population (VAP), 2020: ' + '<strong>' + e.features[0].properties.tvap.toLocaleString("en-US") + '</strong>' +
+        '</br>' + 'Percent Black VAP: ' + '<strong>' + (e.features[0].properties.pct_bvp * 100).toFixed() + '%' + '</strong>' +
+        '</br>' + 'Percent Asian VAP: ' + '<strong>' + (e.features[0].properties.pct_avp * 100).toFixed() + '%' + '</strong>' +
+        '</br>' + 'Percent Hispanic VAP: ' + '<strong>' + (e.features[0].properties.pct_hvp * 100).toFixed() + '%' + '</strong>' +
+        '</br>' + 'Percent Minority VAP: ' + '<strong>' + (e.features[0].properties.pct_bp_ * 100).toFixed() + '%' + '</strong>' +
+        '</br>' + 'Partisan Lean, Percent Democrat 2018-21: ' + '<strong>' + (e.features[0].properties.partisan * 100).toFixed() + '%' + '</strong>'+
+        '</br>' + '<em>' + '* All district populations are estimates based on voting precints' + '</em>';
+    } else {
+        document.getElementById('tooltip-name').innerHTML = "";
+        document.getElementById('tooltip').innerHTML = "";
+    }
+});
+
 map.on("mousemove", "congress_popup", function(e) {
   $('#sidebar').show();
 // create hover effect
@@ -511,6 +542,17 @@ map.on("mouseleave", "house_p1_popup", function() {
 map.on("mouseleave", "house_p2_popup", function() {
 // remove hover color
     map.setFilter("house_p2_hover", ["==", "district", ""]);
+// remove popup
+    popup.remove();  // make this work, and create a hover handler
+    document.getElementById('tooltip').innerHTML = "";
+    map.getCanvas().style.cursor = 'default';
+    $('#sidebar').hide();
+});
+
+// Reset the state-fills-hover layer's filter when the mouse leaves the layer.
+map.on("mouseleave", "house_p3_popup", function() {
+// remove hover color
+    map.setFilter("house_p3_hover", ["==", "district", ""]);
 // remove popup
     popup.remove();  // make this work, and create a hover handler
     document.getElementById('tooltip').innerHTML = "";
@@ -695,7 +737,7 @@ map.addControl(new mapboxgl.NavigationControl({
 
   const leg_layers = [
     'senate',   'senate_p1', 'senate_p2', 'senate_p3',
-    'house',    'house_p1', 'house_p2',
+    'house',    'house_p1', 'house_p2', 'house_p3',
     'congress', 'congress_proposed',
     'congress_proposed_2'
   ];
@@ -710,14 +752,14 @@ map.addControl(new mapboxgl.NavigationControl({
 
   const leg_fill_layers = [
     'senate_fill',    'senate_p1_fill', 'senate_p2_fill', 'senate_p3_fill',
-    'house_fill',     'house_p1_fill', 'house_p2_fill',
+    'house_fill',     'house_p1_fill', 'house_p2_fill', 'house_p3_fill',
     'congress_fill',  'congress_proposed_fill',
     'congress_proposed_2_fill'
   ];
 
   const leg_popup_layers = [
     'senate_popup',    'senate_p1_popup', 'senate_p2_popup', 'senate_p3_popup',
-    'house_popup',     'house_p1_popup', 'house_p2_popup',
+    'house_popup',     'house_p1_popup', 'house_p2_popup', 'house_p3_popup',
     'congress_popup',  'congress_proposed_popup',
     'congress_proposed_2_popup'
   ];
